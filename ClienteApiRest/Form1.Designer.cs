5ebf7e83b89a367c0c14ea2cd2028526f1657c40
Roberto <nicaragua_6002@yahoo.es>
2020-08-21 15:31:15 -0600

    GetApis Ready

--- ClienteApiRest/Form1.Designer.cs
+++ ClienteApiRest/Form1.Designer.cs
@@ -28,21 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txttoken = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // txttoken
+            // 
+            this.txttoken.Location = new System.Drawing.Point(72, 48);
+            this.txttoken.Multiline = true;
+            this.txttoken.Name = "txttoken";
+            this.txttoken.Size = new System.Drawing.Size(556, 236);
+            this.txttoken.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txttoken);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txttoken;
     }
 }
 
--- ClienteApiRest/Form1.cs
+++ ClienteApiRest/Form1.cs
@@ -8,12 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net.Http;
-
+using System.Net.Http.Headers;
 
 namespace ClienteApiRest
 {
     public partial class Form1 : Form
     {
+        EData respuesta;
+        
         private static readonly HttpClient client = new HttpClient();
         public Form1()
         {
@@ -22,25 +24,76 @@ namespace ClienteApiRest
      
         private void Form1_Load(object sender, EventArgs e)
         {
+
+            PostToken();
+            GetPerfil();
+            GetMarcas();
+            GetEstadoCuenta();
+        }
+
+        public void GetPerfil()
+        {
+           
+            txttoken.Text = respuesta.tokenDelUsuario;
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", respuesta.tokenDelUsuario);
+            var response = client.GetAsync("https://sigi.unan.edu.ni/RAServices/Api/PortalEstudiante/GetPortal");
+
+            MessageBox.Show(response.Result.Content.ReadAsStringAsync().Result);
+        }
+
+        public void GetMarcas()
+        {
+
+            txttoken.Text = respuesta.tokenDelUsuario;
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", respuesta.tokenDelUsuario);
+            var response = client.GetAsync("https://sigi.unan.edu.ni/RAServices/Api/PortalEstudiante/GetMarcas");
+
+            MessageBox.Show(response.Result.Content.ReadAsStringAsync().Result);
+        }
+
+        public void GetEstadoCuenta()
+        {
+
+            txttoken.Text = respuesta.tokenDelUsuario;
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", respuesta.tokenDelUsuario);
+            var response = client.GetAsync("https://sigi.unan.edu.ni/RAServices/Api/PortalEstudiante/GetEstadoDeCuenta");
+
+            MessageBox.Show(response.Result.Content.ReadAsStringAsync().Result);
+        }
+
+
+
+
+        //conectarse
+
+        public void PostToken()
+        {
             var values = new Dictionary<string, string>
                 {
-                      { "value", "rsolis1" },
-                 { "id", "1" }
+                      { "UserName", "18000002" },
+                      { "Password", "15061995%" }
                       //{ "Password", "Managua.2020" }
                 };
 
             var content = new FormUrlEncodedContent(values);
 
-            var response =  client.PostAsync("http://localhost:53416/Api/Values", content);
-
-        
+            var response = client.PostAsync("https://sigi.unan.edu.ni/RAServices/Api/Autorizacion/Login", content);
 
 
-            MessageBox.Show(response.Result.Content.ReadAsStringAsync().Result);
+            MessageBox.Show(response.Result.ToString());
+
+            //MessageBox.Show(response.Result.Content.ReadAsStringAsync().Result);
 
 
-           
-
+            respuesta = Newtonsoft.Json.JsonConvert.DeserializeObject<EData>(response.Result.Content.ReadAsStringAsync().Result);
+            
         }
+    }
+
+    class EData
+    {
+
+        public string cuentaDeLUsuario { get; set; }
+        public string tokenDelUsuario { get; set; }
     }
 }
